b752b2f7602f305f69b7558b3e958f72b1045d25
hminhanh <you@example.com>
2019-02-26 15:36:49 +0700

    done core

--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -2,8 +2,9 @@
 
 
 import argparse
+import hashlib
+import json
 import os
-import glob
 
 
 def parse_input():
@@ -18,9 +19,50 @@ def parse_input():
 
 def scan_files(path):
     files = []
-    for dirpath, _, filenames in os.walk(path):
+    for dirpath, _, filenames in os.walk(path, followlinks=False):
         for f in filenames:
             files.append(os.path.abspath(os.path.join(dirpath, f)))
     return files
 
 
+def group_files_by_size(file_path_names):
+    groups = {}
+    for filename in file_path_names:
+        size = os.path.getsize(filename)
+        if size == 0:
+            continue
+        if size in groups.keys():
+            groups[size].append(filename)
+        else:
+            groups[size] = [filename]
+    return list(groups.values())
+
+
+def get_file_checksum(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def group_files_by_checksum(file_path_names):
+    groups = {}
+    for filename in file_path_names:
+        checksum = get_file_checksum(filename)
+        if checksum in groups.keys():
+            groups[checksum].append(filename)
+        else:
+            groups[checksum] = [filename]
+    return list(groups.values())
+
+
+def find_duplicate_files(file_path_names):
+    groups_dup_files = []
+    groups_by_size = group_files_by_size(file_path_names)
+    for gr_by_size in groups_by_size:
+        groups_by_checksum = group_files_by_checksum(gr_by_size)
+        for gr_by_checksum in groups_by_checksum:
+            if len(gr_by_checksum) > 1:
+                groups_dup_files.append(gr_by_checksum)
+    return json.dumps(groups_dup_files)
--- test.py
+++ test.py
@@ -1,26 +1,37 @@
 from find_duplicate_files import *
-
-# paths = scan_files('~/Pictures')
-# print(len(paths))
-# for p in paths:
-#     print(p)
-
-
 import unittest
+import os
 from subprocess import getoutput as run
 
 
 class TestFindDup(unittest.TestCase):
     def setUp(self):
-        run('touch abc xyz')
+        # test scan_files
+        run('mkdir test')
+        os.chdir('test/')
+        run('touch file1 .hidden')
+        run('ln -s file1 symlink1')
+        run('mkdir emptydir')
+        run('ln -s emptydir symlink0')
+        os.chdir('../')
+
+        # test group_files_by_size
+        run('python3 generate_duplicate_files.py -p test')
 
     def tearDown(self):
-        run('rm abc xyz')
+        run('rm -rf test')
 
     def test_scan_files(self):
-        out = str(scan_files('.'))
-        self.assertIn('abc', out)
-        self.assertIn('xyz', out)
+        out = str(scan_files('/home/hminhanh/DuplicateFilesFinder/test'))
+        self.assertIn("file1'", out)
+        self.assertIn("symlink1'", out)
+        self.assertIn(".hidden'", out)
+        self.assertNotIn("emptydir", out)
+        self.assertNotIn("symlink0", out)
+
+    def test_group_files_by_size(self):
+        out = group_files_by_size(scan_files('test'))
+        self.assertIn('?', '?')
 
 
 if __name__ == '__main__':
